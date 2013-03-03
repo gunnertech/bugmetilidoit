0708634472ad480b22d3ee03ac04c082a8c5a81e
Cody Swann <cody@gunnertech.com>
2013-03-02 23:04:03 -0500

    prototype ready

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Bugmetilidoit::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN'].blank? ? 'bugmetilidoit-staging.herokuapp.com' : ENV['DOMAIN'] }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
