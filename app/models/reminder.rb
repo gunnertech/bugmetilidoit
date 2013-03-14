2e2f32b5823ff1e68b9d1e00b3d7d7cf15063d81
Cody Swann <cody@gunnertech.com>
2013-03-13 23:35:01 -0400

    adds SMS

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -11,8 +11,14 @@ class Reminder < ActiveRecord::Base
     ReminderMailer.reminder_email(self,self.user).deliver
     
     if ENV['BLOWERIO_URL']
+      @client = Twilio::REST::Client.new "AC9cb7e9f9a83d6e1b6ab9cb907e89ac85", "9d71acdbf9d13fc82ce048557c3866aa"
+      @client.account.sms.messages.create(
+        :from => '+12038197645',
+        :to => self.user.mobile,
+        :body => "#{@reminder.task.title}. Did it? Click here: #{complete_assigned_task_url(self.assigned_task, auth_token: self.authentication_token, assigned_task: {action: "complete"})}"
+      ) rescue nil
       blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
-      blowerio['/messages'].post :to => '+3018300451', :message => 'Fuck yeah, SMS!' rescue nil
+      blowerio['/messages'].post :to => '+13018300451', :message => 'Fuck yeah, SMS!' rescue nil
     end
     
     self.assigned_task.remind_at = Time.now + self.assigned_task.reminder_frequency.to_i.minutes
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303025254) do
+ActiveRecord::Schema.define(:version => 20130314032904) do
 
   create_table "assigned_tasks", :force => true do |t|
     t.integer  "user_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20130303025254) do
     t.datetime "updated_at",                             :null => false
     t.string   "name"
     t.string   "authentication_token"
+    t.string   "mobile"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
