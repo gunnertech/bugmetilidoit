a91f39c76b9aeda24f89907d29bfd4c0b5286da9
Cody Swann <cody@gunnertech.com>
2014-12-25 00:12:00 -0500

    minor

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::SessionsController < Devise::SessionsController
     respond_to do |format|
       format.html { super }
       format.json {
-        warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+        # warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+        current_user = User.find_or_create_by_facebook_id(params[:user])
+        raise current_user.inspect
         render status: 200, json: current_user.to_json(methods: [:authentication_token])
       }
     end
